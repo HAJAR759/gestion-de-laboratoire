816e388f7aa2d74da8ae5662a48dbf3aab72d594
mouna <mounaettalbi@gmail.com>
2024-10-18 12:34:12 +0100

    modify package entities

--- ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Entities/Laboratoire.java
+++ ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Entities/Laboratoire.java
@@ -1,4 +1,4 @@
-package Entities;
+package com.ProjetLibre.ProjetLibre.Entities;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Entities/Statut.java
+++ ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Entities/Statut.java
@@ -1,4 +1,4 @@
-package Entities;
+package com.ProjetLibre.ProjetLibre.Entities;
 
 public enum Statut {
     ACTIF,
