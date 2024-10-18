cc987c12673efee1e4f10a8474afa47a2a38d311
khadijaoumrani <khadijaoumrani03@gmail.com>
2024-10-18 11:30:31 +0200

    projetBack

---
+++ ProjetLibre/src/test/java/com/ProjetLibre/ProjetLibre/ProjetLibreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetLibre.ProjetLibre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetLibreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
