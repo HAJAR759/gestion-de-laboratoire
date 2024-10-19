6ecc551413ff160d4bf035d50edf784abde423db
khadijaoumrani <khadijaoumrani03@gmail.com>
2024-10-19 18:39:13 +0200

    add Labo controller

--- ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Services/LaboratoireService.java
+++ ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Services/LaboratoireService.java
@@ -2,7 +2,13 @@ package com.ProjetLibre.ProjetLibre.Services;
 
 import com.ProjetLibre.ProjetLibre.Entities.Laboratoire;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface LaboratoireService {
     public void ajouterLaboratoire(Laboratoire laboratoire);
 	Laboratoire modifierLaboratoire(Long id, Laboratoire updatedLaboratoire);
+    public List<Laboratoire> getAllLaboratoires();
+    public Optional<Laboratoire> getLaboratoireById(long id);
+
 }
