cdf3174a947df4ba3b39a60e4eb4fc07214087d0
hajar_elmechhouri <hajarelmech@gmail.com>
2024-10-19 12:56:31 +0100

    lister labo

--- ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Services/LaboratoireServiceImp.java
+++ ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Services/LaboratoireServiceImp.java
@@ -2,6 +2,10 @@ package com.ProjetLibre.ProjetLibre.Services;
 
 import com.ProjetLibre.ProjetLibre.Entities.Laboratoire;
 import com.ProjetLibre.ProjetLibre.repository.LaboratoireRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ProjetLibre.ProjetLibre.Exception.ResourceNotFoundException;
@@ -31,5 +35,14 @@ public class LaboratoireServiceImp implements LaboratoireService{
 	        }).orElseThrow(() -> new ResourceNotFoundException("Laboratoire introuvable avec l'id : " + id));
 	}
 
+	// Récupérer tous les laboratoires
+    public List<Laboratoire> getAllLaboratoires() {
+        return laboratoireRepository.findAll();
+    }
+
+    // Récupérer un laboratoire par ID
+    public Optional<Laboratoire> getLaboratoireById(long id) {
+        return laboratoireRepository.findById(id);
+    }
     
 }
--- ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/controller/LaboratoireController.java
+++ ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/controller/LaboratoireController.java
@@ -1,8 +1,37 @@
 package com.ProjetLibre.ProjetLibre.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import com.ProjetLibre.ProjetLibre.Entities.Laboratoire;
+import com.ProjetLibre.ProjetLibre.Services.LaboratoireServiceImp;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/api/laboratoires")
 public class LaboratoireController {
 
+    @Autowired
+    private LaboratoireServiceImp laboratoireService;
+
+    // Récupérer tous les laboratoires
+    @GetMapping("/all")
+    public List<Laboratoire> getAllLaboratoires() {
+        return laboratoireService.getAllLaboratoires();
+    }
+
+    // Récupérer un laboratoire par ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Laboratoire> getLaboratoireById(@PathVariable long id) {
+        Optional<Laboratoire> laboratoire = laboratoireService.getLaboratoireById(id);
+        return laboratoire.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    
 }
+
