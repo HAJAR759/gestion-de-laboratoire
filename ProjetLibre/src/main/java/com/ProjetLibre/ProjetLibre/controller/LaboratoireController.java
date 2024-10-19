11a34bdd408af91a05b226eebd8bf12741a5f174
mouna <mounaettalbi@gmail.com>
2024-10-19 21:00:33 +0100

    add ModifierLabo in LaboratoireController

--- ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/controller/LaboratoireController.java
+++ ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/controller/LaboratoireController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -18,24 +19,36 @@ public class LaboratoireController {
     @Autowired
     private LaboratoireService laboratoireService;
 
-    // Récupérer tous les laboratoires
+    
     @GetMapping("/all")
     public List<Laboratoire> getAllLaboratoires() {
         return laboratoireService.getAllLaboratoires();
     }
 
-    // Récupérer un laboratoire par ID
+    
     @GetMapping("/{id}")
     public ResponseEntity<Laboratoire> getLaboratoireById(@PathVariable long id) {
         Optional<Laboratoire> laboratoire = laboratoireService.getLaboratoireById(id);
         return laboratoire.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
+    
     @PostMapping("/ajouterLaboratoire")
     public void ajouPrestataire(@RequestBody Laboratoire laboratoire) {
         laboratoireService.ajouterLaboratoire(laboratoire);
     }
 
+    @PutMapping("/modifierLabo/{id}")
+    public ResponseEntity<Laboratoire> modifierLaboratoire(@PathVariable Long id, @RequestBody Laboratoire updatedLaboratoire) {
+        Optional<Laboratoire> existingLaboratoire = laboratoireService.getLaboratoireById(id);
+
+        if (existingLaboratoire.isPresent()) {
+            Laboratoire modifiedLaboratoire = laboratoireService.modifierLaboratoire(id, updatedLaboratoire);
+            return new ResponseEntity<>(modifiedLaboratoire, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
     
 }
 
