29a6c1aa6910ef362bf9e2102ac62f6abfa08487
mouna <mounaettalbi@gmail.com>
2024-10-19 13:19:17 +0100

    commit changes

--- ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Services/LaboratoireService.java
+++ ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Services/LaboratoireService.java
@@ -1,8 +1,13 @@
 package com.ProjetLibre.ProjetLibre.Services;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.ProjetLibre.ProjetLibre.Entities.Laboratoire;
 
 public interface LaboratoireService {
     public void ajouterLaboratoire(Laboratoire laboratoire);
 	Laboratoire modifierLaboratoire(Long id, Laboratoire updatedLaboratoire);
+	public List<Laboratoire> getAllLaboratoires();
+	public Optional<Laboratoire> getLaboratoireById(long id);
 }
