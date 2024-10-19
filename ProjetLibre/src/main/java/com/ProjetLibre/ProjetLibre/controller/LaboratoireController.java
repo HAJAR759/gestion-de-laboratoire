d0ff94a38f994df0337153fb3eb231dc772db84d
khadijaoumrani <khadijaoumrani03@gmail.com>
2024-10-19 14:11:14 +0200

    add Labo controller

--- ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/controller/LaboratoireController.java
+++ ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/controller/LaboratoireController.java
@@ -1,9 +1,21 @@
 package com.ProjetLibre.ProjetLibre.controller;
 
+import com.ProjetLibre.ProjetLibre.Entities.Laboratoire;
+import com.ProjetLibre.ProjetLibre.Services.LaboratoireService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LaboratoireController {
+    @Autowired
+    LaboratoireService laboratoireService;
+
+    @PostMapping("/ajouterLaboratoire")
+    public void ajouConducteur(@RequestBody Laboratoire laboratoire) {
+        laboratoireService.ajouterLaboratoire(laboratoire);
+    }
 
 
 }
---
+++ ProjetLibre/src/test/java/com/ProjetLibre/ProjetLibre/LaboratoireServiceImpTest.java
@@ -0,0 +1,2 @@
+package com.ProjetLibre.ProjetLibre;public class LaboratoireServiceImpTest {
+}
