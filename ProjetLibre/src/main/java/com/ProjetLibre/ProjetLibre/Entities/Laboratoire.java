4e752dd64c6dab5ff4aded23901d4584d61ea174
mouna <mounaettalbi@gmail.com>
2024-10-19 11:31:46 +0100

    Sauvegarde des changements locaux

--- ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Entities/Laboratoire.java
+++ ProjetLibre/src/main/java/com/ProjetLibre/ProjetLibre/Entities/Laboratoire.java
@@ -4,9 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.util.Date;
-import java.util.List;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
