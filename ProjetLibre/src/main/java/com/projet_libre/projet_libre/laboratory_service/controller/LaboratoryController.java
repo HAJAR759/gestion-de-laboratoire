00db1f6653896a9089409580243b86958d9caa1c
hajar_elmechhouri <hajarelmech@gmail.com>
2024-11-06 21:42:32 +0100

    remove Autowired2

--- ProjetLibre/src/main/java/com/projet_libre/projet_libre/laboratory_service/controller/LaboratoryController.java
+++ ProjetLibre/src/main/java/com/projet_libre/projet_libre/laboratory_service/controller/LaboratoryController.java
@@ -1,6 +1,5 @@
 package com.projet_libre.projet_libre.laboratory_service.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
