9c9c41e6259a6039d653741a8ddcb167f8d4ae30
hajar_elmechhouri <hajarelmech@gmail.com>
2024-11-07 21:44:08 +0100

    update MapperTest3

--- ProjetLibre/src/test/java/com/projet_libre/projet_libre/MapperTest.java
+++ ProjetLibre/src/test/java/com/projet_libre/projet_libre/MapperTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 import java.text.ParseException;
-
+import java.text.SimpleDateFormat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MapperTest {
